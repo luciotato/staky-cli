c5d64275d6187fcf0ec3c81979b58ef65c17e6b6
luciotato <luciotato@gmail.com>
2020-12-24 17:14:51 -0300

    initial

---
+++ CLIConfig.js
@@ -0,0 +1,7 @@
+
+        module.exports = {
+            nickname: "staky",
+            userAccount: "luciotato.guildnet",
+            contractAccount: "staking-pool-4.guildnet"
+        }
+        
