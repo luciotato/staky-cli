79a3212fb15ca7ce30fd484613cddd7a8e3847e6
luciotato <luciotato@gmail.com>
2020-12-29 05:38:54 -0300

    add quotes to json params

--- CLIConfig.js
+++ CLIConfig.js
@@ -2,6 +2,5 @@
         module.exports = {
             nickname: "staky",
             userAccount: "luciotato.guildnet",
-            contractAccount: "staking-pool-4.guildnet"
+            contractAccount: "staking-pool-2.guildnet"
         }
-        
